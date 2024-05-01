6f067c28edb56d0cc0c7105f8c19e69dc466d3ee
Enes Kabakaya <151635424+EnesKabakaya1@users.noreply.github.com>
2024-05-01 13:01:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,21 +11,27 @@ app.use(cors());
 app.post('/lead-collection', [
     body('email').isEmail(),
     body('phone').isLength({ min: 10, max: 10 }).isNumeric(),
-], (req, res) => {
+], async(req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: 'Invalid Data' });
     }
 
+    await fakeDelay(1000);
+
     const couponCode = getCouponCode();
 
-    setTimeout(() => res.status(200).json({ couponCode }), 2000);
+    res.status(200).json({ couponCode });
 });
 
 const getCouponCode = () => {   
     return 'COUPON_2024';
 };
+
+const fakeDelay = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running http://localhost:${ PORT }`);
